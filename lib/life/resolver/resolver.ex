22f5c173e16483940757bfb73098ac3e7912b49e
Mathew Bramson <mathewbramson@gmail.com>
2019-04-07 15:21:56 -0400

    Remove rows from Resolver

--- lib/life/resolver/resolver.ex
+++ lib/life/resolver/resolver.ex
@@ -6,28 +6,27 @@ defmodule Life.Resolver do
   alias Life.Resolver.UnrolledGrid
 
   def step(grid) do
-    rows = rows_in(grid)
     columns = columns_in(grid)
 
     unrolled_grid_tuple = UnrolledGrid.unroll(grid)
 
     next_step_unrolled_grid =
-      calculate_neighbors(unrolled_grid_tuple, rows, columns)
+      calculate_neighbors(unrolled_grid_tuple, columns)
 
     UnrolledGrid.roll(next_step_unrolled_grid, columns)
   end
 
-  defp calculate_neighbors(unrolled_grid, rows, columns) do
+  defp calculate_neighbors(unrolled_grid, columns) do
     unrolled_grid
-    |> reduce_grid(0, rows, columns, [])
+    |> reduce_grid(0, columns, [])
     |> Enum.reverse
   end
 
-  defp reduce_grid(unrolled_grid, current_cell, _, _, acc)
+  defp reduce_grid(unrolled_grid, current_cell, _, acc)
     when tuple_size(unrolled_grid) == current_cell do
     acc
   end
-  defp reduce_grid(unrolled_grid, current_cell, rows, columns, acc) do
+  defp reduce_grid(unrolled_grid, current_cell, columns, acc) do
     current_cell_value = elem(unrolled_grid, current_cell)
 
     neighbors =
@@ -47,10 +46,8 @@ defmodule Life.Resolver do
         0
       end
 
-    reduce_grid(unrolled_grid, current_cell + 1, rows, columns, [next_step_cell_value | acc])
+    reduce_grid(unrolled_grid, current_cell + 1, columns, [next_step_cell_value | acc])
   end
-
-  defp rows_in(grid), do: length(grid)
 
   defp columns_in([]), do: 0
   defp columns_in([row | _]), do: length(row)
