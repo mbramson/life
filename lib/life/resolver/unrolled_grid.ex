28c2af72848d39edf11008be48d2d6ab7e89c0f4
Mathew Bramson <mathewbramson@gmail.com>
2019-04-07 15:15:05 -0400

    Refactor UnrolledGrid

--- lib/life/resolver/unrolled_grid.ex
+++ lib/life/resolver/unrolled_grid.ex
@@ -12,75 +12,90 @@ defmodule Life.Resolver.UnrolledGrid do
   end
 
   def cell_to_right({}, _, _), do: 0
-  def cell_to_right(unrolled_grid, current_cell_number, columns) do
-    if rem(current_cell_number, columns) == columns - 1 do
-      # cell is on right border
+  def cell_to_right(unrolled_grid, position, columns) do
+    if on_right_border(position, columns) do
       0
     else
-      elem(unrolled_grid, current_cell_number + 1)
+      elem(unrolled_grid, position + 1)
     end
   end
 
   def cell_to_left({}, _, _), do: 0
-  def cell_to_left(unrolled_grid, current_cell_number, columns) do
-    if rem(current_cell_number, columns) == 0 do
+  def cell_to_left(unrolled_grid, position, columns) do
+    if on_left_border(position, columns) do
       0
     else
-      elem(unrolled_grid, current_cell_number - 1)
+      elem(unrolled_grid, position - 1)
     end
   end
 
   def cell_above({}, _, _), do: 0
-  def cell_above(unrolled_grid, current_cell_number, columns) do
-    if current_cell_number < columns do
+  def cell_above(unrolled_grid, position, columns) do
+    if on_top_border(position, columns) do
       0
     else
-      elem(unrolled_grid, current_cell_number - columns)
+      elem(unrolled_grid, position - columns)
     end
   end
 
   def cell_below({}, _, _), do: 0
-  def cell_below(unrolled_grid, current_cell_number, columns) do
-    if current_cell_number >= tuple_size(unrolled_grid) - columns do
+  def cell_below(unrolled_grid, position, columns) do
+    if on_lower_border(position, columns, unrolled_grid) do
       0
     else
-      elem(unrolled_grid, current_cell_number + columns)
+      elem(unrolled_grid, position + columns)
     end
   end
 
   def cell_above_left({}, _, _), do: 0
-  def cell_above_left(unrolled_grid, current_cell_number, columns) do
-    if current_cell_number < columns or rem(current_cell_number, columns) == 0 do
+  def cell_above_left(unrolled_grid, position, columns) do
+    if on_top_border(position, columns) or on_left_border(position, columns) do
       0
     else
-      elem(unrolled_grid, current_cell_number - columns - 1)
+      elem(unrolled_grid, position - columns - 1)
     end
   end
 
   def cell_above_right({}, _, _), do: 0
-  def cell_above_right(unrolled_grid, current_cell_number, columns) do
-    if current_cell_number < columns or rem(current_cell_number, columns) == columns - 1 do
+  def cell_above_right(unrolled_grid, position, columns) do
+    if on_top_border(position, columns) or on_right_border(position, columns) do
       0
     else
-      elem(unrolled_grid, current_cell_number - columns + 1)
+      elem(unrolled_grid, position - columns + 1)
     end
   end
 
   def cell_below_left({}, _, _), do: 0
-  def cell_below_left(unrolled_grid, current_cell_number, columns) do
-    if current_cell_number >= tuple_size(unrolled_grid) - columns or rem(current_cell_number, columns) == 0 do
+  def cell_below_left(unrolled_grid, position, columns) do
+    if on_lower_border(position, columns, unrolled_grid) or on_left_border(position, columns) do
       0
     else
-      elem(unrolled_grid, current_cell_number + columns - 1)
+      elem(unrolled_grid, position + columns - 1)
     end
   end
 
   def cell_below_right({}, _, _), do: 0
-  def cell_below_right(unrolled_grid, current_cell_number, columns) do
-    if current_cell_number >= tuple_size(unrolled_grid) - columns or rem(current_cell_number, columns) == columns - 1 do
+  def cell_below_right(unrolled_grid, position, columns) do
+    if on_lower_border(position, columns, unrolled_grid) or on_right_border(position, columns) do
       0
     else
-      elem(unrolled_grid, current_cell_number + columns + 1)
+      elem(unrolled_grid, position + columns + 1)
     end
+  end
+
+  defp on_right_border(position, columns) do
+    rem(position, columns) == columns - 1
+  end
+
+  defp on_left_border(position, columns) do
+    rem(position, columns) == 0
+  end
+
+  defp on_top_border(position, columns) do
+    position < columns
+  end
+
+  defp on_lower_border(position, columns, unrolled_grid) do
+    position >= tuple_size(unrolled_grid) - columns
   end
 end
